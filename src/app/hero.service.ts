266ea8d313caec45ca55b3f81dd98c4ebf5e5c12
jcroa <jcarlos.roa@gmail.com>
2018-04-04 19:13:19 +0200

    Http. GetHeroes and GetHero
    * call http when  in-memory-data-service is enabled
    * handle error with message and returning safe data

--- src/app/hero.service.ts
+++ src/app/hero.service.ts
@@ -4,13 +4,16 @@ import { Observable } from 'rxjs/Observable';
 import { of } from 'rxjs/observable/of';
 
 import { Hero } from './hero';
-import { HEROES } from './mock-heroes';
+//import { HEROES } from './mock-heroes';
 import { HttpClient, HttpHeaders } from '@angular/common/http';
+import { catchError, map, tap } from 'rxjs/operators';
 
 import { MessageService } from './message.service';
 
 @Injectable()
 export class HeroService {
+
+  private heroesUrl = 'api/heroes';  // URL to web api
 
   constructor(
     private http: HttpClient,
@@ -20,14 +23,55 @@ export class HeroService {
   /* Returns an Observable object for getting of list of all heroes */
   getHeroes(): Observable<Hero[]> {
     // Todo: send the message _after_ fetching the heroes
-    this.messageService.add('HeroService: fetched heroes');
-    return of(HEROES);
+
+    //this.messageService.add('HeroService: fetched heroes');
+    //return of(HEROES);
+
+    return this.http.get<Hero[]>(this.heroesUrl)
+      .pipe(
+        tap(heroes => this.log(`fetched heroes`)),
+        catchError(this.handleErrorDelegate("getHeroes", []))
+      );
   }
 
   getHero(id: number): Observable<Hero> {
     // Todo: send the message _after_ fetching the hero
-    this.messageService.add(`HeroService: fetched hero id=${id}`);
-    return of(HEROES.find(hero => hero.id === id));
+
+    //this.messageService.add(`HeroService: fetched hero id=${id}`);
+    //return of(HEROES.find(hero => hero.id === id));
+
+    const url = `${this.heroesUrl}/${id}`;
+    return this.http.get<Hero>(url)
+      .pipe(
+        tap(_ => this.log(`fetched hero id=${id}`)),
+        catchError(this.handleErrorDelegate<Hero>(`getHero id=${id}`))
+      )
+  }
+
+  /**
+   * Handle Http operation that failed.
+   * Let the app continue.
+   * @param operation - name of the operation that failed
+   * @param result - optional value to return as the observable result
+   */
+  handleErrorDelegate<T>(operationName = 'operation', result?: T) {
+    // return a function that will handle the error
+    return (error: any): Observable<T> => {
+
+      // TODO: send the error to remote logging infrastructure
+      console.error(error); // log to console instead
+  
+      // TODO: better job of transforming error for user consumption
+      this.log(`${operationName} failed: ${error.message}`);
+  
+      // Let the app keep running by returning an empty result.
+      return of(result as T);
+    };
+  }
+
+  /** Log a HeroService message with the MessageService */
+  private log(message: string) {
+    this.messageService.add('HeroService: ' + message);
   }
 
 }
